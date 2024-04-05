bfc06385d62c1b47e1be1d6bbccfc2cd799fc7f2
ErnestoHV <ernesto.hernandezv@alumno.buap.mx>
2024-04-05 15:33:31 -0600

    Reseteo del proyecto

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,10 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
-
-//Rutas para tener acceso al sistema (Login)
-$routes->match(['get', 'post'], 'SigninController/loginAuth', 'SigninController::loginAuth');
-$routes->get('usuarios', 'Usuarios::index');
-$routes->get('salir', 'SigninController::logout');
-$routes->get('login', 'SigninController::index');
