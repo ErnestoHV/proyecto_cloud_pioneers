c96010b6d5e92c5971705b7e86527ae8797e72ca
ErnestoHV <ernesto.hernandezv@alumno.buap.mx>
2024-04-24 15:06:08 -0600

    Se añadieron campos a tomar de la tabla Usuario dentro del modelo

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,7 +7,7 @@ class UserModel extends Model
 {
     protected $table      = 'usuario';
     protected $primaryKey = 'id_usuario';
-    protected $allowedFields = ['id_rol','contrasena_usuario','correo_electronico','nombre_usuario','apellidos_usuario','fecha_creacion_usuario','fecha_modificacion_usuario'];
+    protected $allowedFields = ['id_rol','contrasena_usuario','correo_electronico','nombre_usuario','apellidos_usuario','fecha_creacion_usuario','fecha_modificacion_usuario','fecha_borrado_usuario','estado_registro_usuario'];
 
     protected $returnType = 'array';
     protected $useSoftDeletes = true;
@@ -16,6 +16,7 @@ class UserModel extends Model
     protected $dateFormat = 'datetime';
     protected $createdField = 'fecha_creacion_usuario';
     protected $updatedField = 'fecha_modificacion_usuario';
+    protected $deletedField = 'fecha_borrado_usuario';
     
 }
 
